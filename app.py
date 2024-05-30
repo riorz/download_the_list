304a493ff4b1a27a0d92695fad75dd5752dc8f1e
riorz <reevizy@gmail.com>
2024-05-31 04:39:53 +0800

    added download video option

--- app.py
+++ app.py
@@ -21,23 +21,42 @@ def get_highest_quality(v: YouTube)->None:
     combine_video(video, audio)
 
 
-_base_url = 'https://www.youtube.com/playlist?list='
-pid = questionary.text('playlist id').ask()
-p = Playlist(f'{_base_url}{pid}')
-print(f'{p.title}: {p.length} videos')
-is_getting_highest = questionary.confirm('Getting highest quality?').ask()
-required_word = questionary.text('Included word:').ask()
-fetch_list = p.videos
-if required_word:
-    print(f'including word: {required_word}')
-    fetch_list = [v for v in p.videos if required_word in v.title]
-    for i in fetch_list:
-        print(i.title)
+_playlist_base_url = 'https://www.youtube.com/playlist?list='
+_video_base_url = 'http://www.youtube.com/watch?v='
 
-if is_getting_highest:
-    for v in fetch_list:
+
+if __name__ == '__main__':
+    answers = questionary.form(
+        dl_type = questionary.rawselect(
+            'Download type',
+            choices = ['playlist', 'video'
+        ]),
+        id = questionary.text('id')
+    ).ask()
+
+    id = answers['id']
+    if answers['dl_type'] == 'playlist':
+        p = Playlist(f'{_playlist_base_url}{id}')
+        print(f'{p.title}: {p.length} videos')
+
+        is_getting_highest = questionary.confirm('Getting highest quality?').ask()
+        required_word = questionary.text('Included word:').ask()
+        fetch_list = p.videos
+        if required_word:
+            print(f'including word: {required_word}')
+            fetch_list = [v for v in p.videos if required_word in v.title]
+            for i in fetch_list:
+                print(i.title)
+
+        if is_getting_highest:
+            for v in fetch_list:
+                get_highest_quality(v)
+        else:
+            for v in fetch_list:
+                # download highest progressive videos
+                v.streams.get_highest_resolution().download()
+
+    else:
+        v = YouTube(f'{_video_base_url}{id}')
+        print(v.title)
         get_highest_quality(v)
-else:
-    for v in fetch_list:
-        # download highest progressive videos
-        v.streams.get_highest_resolution().download()
